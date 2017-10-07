56fff8f9e6f0f53a54be02700afe94082ccfb4f8
Pooja Reddy <pooja.gayam@gmail.com>
2017-10-07 18:37:25 +0530

    Create 2.16.c

---
+++ 2.16.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(void){
+int num1,num2;
+printf("Enter two numbers: ");
+scanf("%d %d",&num1,&num2);
+printf("Sum of given numbers:%d \n",num1 + num2);
+printf("Difference of given numbers:%d \n",num1 - num2);
+printf("Product of given numbers:%d \n",num1 * num2);
+printf("Division of given numbers:%d \n",num1 / num2);
+printf("Multiplication of given numbers:%d \n",num1 % num2);
+}
